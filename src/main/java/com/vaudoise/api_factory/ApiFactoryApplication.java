7aace45aace919ceea527a1dafa577865aa475f8
w.antar <w.antar@romulus.live>
2025-10-12 16:53:21 +0100

    Initial commit

---
+++ src/main/java/com/vaudoise/api_factory/ApiFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.vaudoise.api_factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFactoryApplication.class, args);
+	}
+
+}
